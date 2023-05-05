a285f71f96c473f287e0a0449f88bd67e6561e67
HaMa <vanha.jungle@gmail.com>
2023-05-05 11:20:15 +0700

    font sizing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -51,12 +51,9 @@ const PostView = (props: PostWithUser) => {
       <div className="flex flex-col">
         <div className="flex gap-1 text-slate-300">
           <span className="font-bold">@{author.username}</span>
-          <span className="font-thin">
-            {" "}
-            - {dayjs(post.createdAt).fromNow()}
-          </span>
+          <span className="font-thin">{dayjs(post.createdAt).fromNow()}</span>
         </div>
-        <span>{post.content}</span>
+        <span className="text-xl">{post.content}</span>
       </div>
     </div>
   );
